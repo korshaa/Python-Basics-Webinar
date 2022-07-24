dddf00f2d0b2649bee459da9929d49633f3132fa
korshaa <90961244+korshaa@users.noreply.github.com>
2022-07-24 14:18:10 +0300

    Lesson07 (#3)
    * Задание 6
    
    * Задание 6.2
    
    * Задание 7.1
    
    * удаление
    
    * Задание 7.1
    
    * Задание 7.2-7.3
    
    * Передал объект, замечание преподавателя

--- Lesson 6/lesson06.2.py
+++ Lesson 6/lesson06.2.py
@@ -5,8 +5,8 @@ class Road:
 
     def rez_mass(self, mass_square, thick):
         print(f'{self._length}м * {self._width}м * {mass_square}кг * {thick}см = '
-              f'{self._length * self._width * mass_square * 5 / 1000} т.')
 
+              f'{self._length * self._width * mass_square * thick / 1000} т.')
 
 tl_light = Road(20, 5000)
 tl_light.rez_mass(25, 5)
